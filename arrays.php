307bb18fd0057c9a72a37bfa0278782c578d1a95
trplll <trplll@users.noreply.github.com>
2017-04-10 10:47:11 -0500

    Update arrays.php

--- arrays.php
+++ arrays.php
@@ -43,3 +43,19 @@ $cars=array
   array("BMW",60,59),
   array("Toyota",110,100)
   );
+
+#Change all keys in an array to uppercase:
+#---------------------------------------------------------------------------------------------------------------------
+$age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+print_r(array_change_key_case($age,CASE_UPPER));
+
+#Change all keys in an array to lowercase:
+#---------------------------------------------------------------------------------------------------------------------
+$age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+print_r(array_change_key_case($age,CASE_LOWER));
+
+#If two or more keys will be equal after running array_change_key_case() (e.g. "b" and "B"), the latest array will override the other:
+#---------------------------------------------------------------------------------------------------------------------
+$pets=array("a"=>"Cat","B"=>"Dog","c"=>"Horse","b"=>"Bird");
+print_r(array_change_key_case($pets,CASE_UPPER));
+
