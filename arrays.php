a77a97d36251765b58c5983f5e72a456bbf31365
trplll <trplll@users.noreply.github.com>
2017-04-10 10:52:59 -0500

    Update arrays.php

--- arrays.php
+++ arrays.php
@@ -136,6 +136,34 @@ Array
 )
 
 */
+#Create an array by using the elements from one "keys" array and one "values" array:
+#---------------------------------------------------------------------------------------------------------------------
+$fname=array("Peter","Ben","Joe");
+$age=array("35","37","43");
 
+$c=array_combine($fname,$age);
+print_r($c);
+
+#Count all the values of an array:
+#---------------------------------------------------------------------------------------------------------------------
+$a=array("A","Cat","Dog","A","Dog");
+print_r(array_count_values($a));
+
+#Compare the values of two arrays, and return the differences:
+#---------------------------------------------------------------------------------------------------------------------
+$a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
+$a2=array("e"=>"red","f"=>"green","g"=>"blue");
+
+$result=array_diff($a1,$a2);
+print_r($result);
+
+#Compare the values of three arrays, and return the differences:
+#---------------------------------------------------------------------------------------------------------------------
+$a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
+$a2=array("e"=>"red","f"=>"black","g"=>"purple");
+$a3=array("a"=>"red","b"=>"black","h"=>"yellow");
+
+$result=array_diff($a1,$a2,$a3);
+print_r($result);
 
 
