b77ed55881e1e6837e4dbc645e4251c7b11cacb6
trplll <trplll@users.noreply.github.com>
2017-04-10 10:49:23 -0500

    Update arrays.php

--- arrays.php
+++ arrays.php
@@ -59,3 +59,49 @@ print_r(array_change_key_case($age,CASE_LOWER));
 $pets=array("a"=>"Cat","B"=>"Dog","c"=>"Horse","b"=>"Bird");
 print_r(array_change_key_case($pets,CASE_UPPER));
 
+#Split an array into chunks of two:
+#---------------------------------------------------------------------------------------------------------------------
+$cars=array("Volvo","BMW","Toyota","Honda","Mercedes","Opel");
+print_r(array_chunk($cars,2));
+
+#Split an array into chunks of two and preserve the original keys:
+#---------------------------------------------------------------------------------------------------------------------
+$age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43","Harry"=>"50");
+print_r(array_chunk($age,2,true));
+
+#Get column of last names from a recordset:
+#---------------------------------------------------------------------------------------------------------------------
+// An array that represents a possible record set returned from a database
+$a = array(
+  array(
+    'id' => 5698,
+    'first_name' => 'Peter',
+    'last_name' => 'Griffin',
+  ),
+  array(
+    'id' => 4767,
+    'first_name' => 'Ben',
+    'last_name' => 'Smith',
+  ),
+  array(
+    'id' => 3809,
+    'first_name' => 'Joe',
+    'last_name' => 'Doe',
+  )
+);
+
+$last_names = array_column($a, 'last_name');
+print_r($last_names);
+
+/* 
+Output:
+Array
+(
+  [0] => Griffin
+  [1] => Smith
+  [2] => Doe
+)
+*/
+
+
+
