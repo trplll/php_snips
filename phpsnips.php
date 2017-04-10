9b267de3fb37c455bdad07b877f71fe66ac49e33
trplll <trplll@users.noreply.github.com>
2017-04-10 10:23:24 -0500

    Update and rename phpsnips.txt to phpsnips.php

---
+++ phpsnips.php
@@ -0,0 +1,22 @@
+<?php 
+for ($x = 0; $x <= 10; $x++) {
+    echo "The number is: $x <br>";
+} 
+?>
+
+<?php 
+$colors = array("red", "green", "blue", "yellow"); 
+
+foreach ($colors as $value) {
+    echo "$value <br>";
+}
+?>
+
+<?php 
+$x = 1; 
+
+while($x <= 5) {
+    echo "The number is: $x <br>";
+    $x++;
+} 
+?>
