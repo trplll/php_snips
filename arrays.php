e7f00347900842dfcf20362467a43e86d0c5f57b
trplll <trplll@users.noreply.github.com>
2017-04-10 10:45:18 -0500

    Update arrays.php

--- arrays.php
+++ arrays.php
@@ -1,19 +1,19 @@
 <?php
 
 #Create an indexed array named $cars, assign three elements to it, and then print a text containing the array values:
-
+#---------------------------------------------------------------------------------------------------------------------
 $cars=array("Volvo","BMW","Toyota");
 echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
 
 #Create an associative array named $age:
-
+#---------------------------------------------------------------------------------------------------------------------
 
 $age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
 echo "Peter is " . $age['Peter'] . " years old.";
 
 
 #Loop through and print all the values of an indexed array:
-
+#---------------------------------------------------------------------------------------------------------------------
 $cars=array("Volvo","BMW","Toyota");
 $arrlength=count($cars);
 
@@ -25,7 +25,7 @@ for($x=0;$x<$arrlength;$x++)
   
   
 #Loop through and print all the values of an associative array:  
-
+#---------------------------------------------------------------------------------------------------------------------
 $age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
 
 foreach($age as $x=>$x_value)
@@ -35,7 +35,7 @@ foreach($age as $x=>$x_value)
   }
 
 #Create a multidimensional array:
-
+#---------------------------------------------------------------------------------------------------------------------
 // A two-dimensional array:
 $cars=array
   (
